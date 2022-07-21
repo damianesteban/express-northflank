445469f2ecb0ddb32c3d9d41e4f00e1930a0113d
Damian Esteban <damian.esteban@gmail.com>
2022-07-21 16:29:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-// const express = require('express');
-// const app = express();
-// const port = 80;
+const express = require('express');
+const app = express();
+const port = 80;
 
-// app.get('/', (req, res) => {
-// 	res.send('Hello World!');
-// });
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
 
-// app.get('/api/:id', (req, res) => {
-// 	res.gauch(`Get resource for ${req.params.id}.`);
-// });
+app.get('/api/:id', (req, res) => {
+  if (req.params.balls) {
+    res.send(`Get resource for ${req.params.id}.`);
+  }
+	
+});
 
-// app.listen(port, () => {
-// 	console.log(`Example app listening at http://localhost:${port}`);
-// });
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
